a66996035f15a6f825f871b2e50dbae71ce90ead
Kim Patro <kimpastro@gmail.com>
2021-03-23 08:53:10 -0300

    Initial commit

---
+++ lib/aarin.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "aarin/version"
+
+module Aarin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/aarin_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Aarin do
+  it "has a version number" do
+    expect(Aarin::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
